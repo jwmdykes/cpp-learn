4139e177f6bb8c5361a56c1deacfec42d5366c6b
ulazy1 <ulazy1@github.com>
2022-01-26 13:36:31 -0500

    basic glfw example

--- main.cpp
+++ main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 #include "Adder/adder.h"
+#include "external/glfw/include/GLFW/glfw3.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    GLFWwindow *window;
 
-    cout << add(72.1f, 11.6f) << "\n";
+    int width, height;
+
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+    if (!window)
+    {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
