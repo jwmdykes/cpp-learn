cb0ff73151420cff06c87627b109e1805f95b61b
ulazy198 <ulazy198@gmail.com>
2022-01-26 12:53:02 -0500

    add adder function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+float add(float a, float b);
+
 using namespace std;
 
 int main()
 {
     cout << "Hello World!\n";
+
+    cout << add(72.1f, 11.6f) << "\n";
+
     return 0;
 }
