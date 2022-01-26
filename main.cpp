a419cf9b331ff8082466b01fd45eb4d73b70817a
ulazy1 <ulazy1@github.com>
2022-01-26 15:55:01 -0500

    using openssl example

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
 #include "Adder/adder.h"
 #include "external/glfw/include/GLFW/glfw3.h"
+#include <openssl/sha.h>
 
 using namespace std;
 
-int main()
+int emptyGLFWWindow()
 {
     GLFWwindow *window;
-
-    int width, height;
 
     if (!glfwInit())
     {
@@ -35,5 +38,30 @@ int main()
 
     glfwTerminate();
 
+    return 0;
+}
+
+string sha256(const string str)
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
+
+int main()
+{
+    cout << sha256("1234567890_1") << endl;
+    cout << sha256("1234567890_2") << endl;
+    cout << sha256("1234567890_3") << endl;
+    cout << sha256("1234567890_4") << endl;
+    // emptyGLFWWindow();
     return 0;
 }
